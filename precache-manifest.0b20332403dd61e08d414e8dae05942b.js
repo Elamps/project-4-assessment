f12c0b8eafe092283c9a0394c8a541c65ec1beee
Marcus Lampley <Marcus@Kathleens-Air.hsd1.ga.comcast.net>
2020-09-22 13:59:41 -0400

    Updates

---
+++ precache-manifest.0b20332403dd61e08d414e8dae05942b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6275135f64080c4108e0db594ea4d9ba",
+    "url": "/Elamps/project-4-assessment.git/index.html"
+  },
+  {
+    "revision": "90cd28e17ceac4fe5a40",
+    "url": "/Elamps/project-4-assessment.git/static/css/main.2ddb3bd6.chunk.css"
+  },
+  {
+    "revision": "9bcf3c0bee231d1b9932",
+    "url": "/Elamps/project-4-assessment.git/static/js/2.383b629d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Elamps/project-4-assessment.git/static/js/2.383b629d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90cd28e17ceac4fe5a40",
+    "url": "/Elamps/project-4-assessment.git/static/js/main.b17d08b8.chunk.js"
+  },
+  {
+    "revision": "054f9493bee3cb7b48d9",
+    "url": "/Elamps/project-4-assessment.git/static/js/runtime-main.70bbf9e6.js"
+  }
+]);
